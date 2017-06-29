798114536268ac1af8611e78b2c04b904068d8a6
john mauran <johnmauran@johns-MacBook-Air.local>
2017-06-29 17:03:03 -0400

    set up test one

---
+++ tests/gamelogic_spec.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import GameContainer from 'react/containers/GameContainer';
+import path from 'path';
+
+describe('Testing_game', () => {
+  let score, card, header, button;
+  var player_num = 
+
+  beforeEach(() => {
+    wrapper = mount(
+      <GameContainer
+      score=""
+      card=""
+      header=""
+      button=""
+    )
+  })
+  it('should say battle royale', () => {
+
+  });
+  it('should give the user option to start game', () => {
+
+  });
+  it('screen should show score', () => {
+
+  });
+  it('image of each card should show', () => {
+    expect().toBe();
+  });
+  it('')
+})
